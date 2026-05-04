9e494d23c82752f931123c199a4f7428cf0f7e21
starfrost013 <mario64crashed@gmail.com>
2026-05-04 23:24:50 +0100

    add a basic site skin

---
+++ site/site.py
@@ -0,0 +1,13 @@
+# Core Site
+
+import streamlit as st 
+
+def run():
+    st.set_page_config(
+        page_title = "Welcome to the Energy Engine"
+    )
+
+
+
+if __name__ == "__main__":
+    run()
