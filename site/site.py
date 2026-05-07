b2ca6aa7067246a6e05a202ccb6441973bfbd206
starfrost013 <mario64crashed@gmail.com>
2026-05-07 12:14:53 +0100

    sidebar

--- site/site.py
+++ site/site.py
@@ -2,12 +2,20 @@
 
 import streamlit as st 
 
+PAGE_DASHBOARD = "Dashboard"
+PAGE_LOGIN = "Login"
+PAGE_SIGNUP = "Signup"
+
 def run():
     st.set_page_config(
         page_title = "Welcome to the Energy Engine"
     )
 
-
+    with st.sidebar:
+        st.link_button(PAGE_DASHBOARD, "pages/dashboard")
+        st.link_button(PAGE_LOGIN, "pages/login")
+        st.link_button(PAGE_SIGNUP, "pages/signup")
+        
 
 if __name__ == "__main__":
     run()
